6b81693df7798ee9d16ec7cf5fc0c020d6a9c0eb
Ansar <met2ansar@gmail.com>
2016-12-17 15:39:45 -0800

    new check in

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function() {
@@ -18,3 +22,20 @@ app.listen(app.get('port'), function() {
 });
 
 
+var login = require('./routes/login');
+
+app.use('/login', login);
+
+/* adding HTML as the template view  */
+var fs = require('fs') // this engine requires the fs module
+app.engine('html', function (filePath, options, callback) { // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(err)
+    // this is an extremely simple template engine
+    var rendered = content.toString().replace('#title#', '<title>' + options.title + '</title>')
+    .replace('#message#', '<h1>' + options.message + '</h1>')
+    return callback(null, rendered)
+  })
+})
+//app.set('views', './views') // specify the views directory
+app.set('view engine', 'html') // register the template engine
